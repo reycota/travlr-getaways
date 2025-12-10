63a83d4f89a967b0eb4b3d05ca294e467c171115
Rey Cota <rey.cota@gmail.com>
2025-12-09 23:59:01 -0800

    Module 6 complete - full CRUD + Bootstrap

--- angular-src/src/app/trip-edit/trip-edit.ts
+++ angular-src/src/app/trip-edit/trip-edit.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgIf, CommonModule } from '@angular/common';
@@ -12,36 +12,38 @@ import { Trip, TripData } from '../trip-data';
   styleUrls: ['./trip-edit.css']
 })
 export class TripEdit implements OnInit {
-  trip?: Trip;
+  trip!: Trip;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private tripService: TripData
+    private tripService: TripData,
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
-    const tripId = this.route.snapshot.paramMap.get('tripId');
-    if (!tripId) {
-      this.router.navigate(['/']);
-      return;
-    }
-
-    this.tripService.getTrip(tripId).subscribe({
-      next: (trip: Trip) => this.trip = trip,
-      error: () => this.router.navigate(['/'])
+    this.route.paramMap.subscribe(params => {
+      const tripId = params.get('tripId');
+      if (tripId) {
+        this.tripService.getTrip(tripId).subscribe({
+          next: (trip: Trip) => {
+            this.trip = trip;
+            this.cdr.detectChanges();   
+          },
+          error: () => this.router.navigate(['/'])
+        });
+      }
     });
   }
 
   saveTrip(): void {
-    if (!this.trip?._id) return;
+    if (!this.trip._id) return;
 
     this.tripService.updateTrip(this.trip._id, this.trip).subscribe({
       next: () => {
         this.tripService.triggerRefresh();
         this.router.navigate(['/']);
-      },
-      error: () => alert('Update failed')
+      }
     });
   }
 
